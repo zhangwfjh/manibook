029e1ce8be9dea14112088c202282e0e55a985d8
Shaun Zhang <zhangwfjh@gmail.com>
2025-11-05 18:16:23 +0800

    Add dock with theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
-import { ModeToggle } from "@/components/theme-toggle";
 import {
   Empty,
   EmptyDescription,
   EmptyHeader,
   EmptyTitle,
 } from "@/components/ui/empty";
+import { DockMenu } from "@/components/dock-menu";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="absolute right-4 top-4">
-        <ModeToggle />
-      </div>
       <Empty>
         <EmptyHeader>
-          <EmptyTitle>Welcome to shagent</EmptyTitle>
+          <EmptyTitle>Welcome to Shagent</EmptyTitle>
           <EmptyDescription>
             This is Shaun&#39;s personal AI assistant.
           </EmptyDescription>
         </EmptyHeader>
       </Empty>
+      <DockMenu />
     </div>
   );
 }
---
+++ components/dock-menu.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { HomeIcon, SunMoonIcon } from "lucide-react";
+import {
+  Dock,
+  DockIcon,
+  DockItem,
+  DockLabel,
+} from "@/components/ui/shadcn-io/dock";
+import { useTheme } from "next-themes";
+
+export function DockMenu() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="absolute bottom-2 left-1/2 max-w-full -translate-x-1/2">
+      <Dock className="items-end pb-3">
+        <DockItem
+          className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          <DockLabel>Home</DockLabel>
+          <DockIcon>
+            <HomeIcon className="h-full w-full text-neutral-600 dark:text-neutral-300" />
+          </DockIcon>
+        </DockItem>
+        <DockItem
+          className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800"
+          onClick={() => {
+            if (theme === "dark") {
+              setTheme("light");
+            } else {
+              setTheme("dark");
+            }
+          }}
+        >
+          <DockLabel>Theme</DockLabel>
+          <DockIcon>
+            <SunMoonIcon className="h-full w-full text-neutral-600 dark:text-neutral-300" />
+          </DockIcon>
+        </DockItem>
+      </Dock>
+    </div>
+  );
+}
