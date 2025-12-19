0355c1028e96f7b74d5547a0bfb2f06ad36c29c0
Shaun Zhang <zhangwfjh@gmail.com>
2025-12-19 22:15:43 +0800

    wip: try document classification

--- app/library/metadata.tsx
+++ app/library/metadata.tsx
@@ -1,7 +1,8 @@
 import mupdf from "mupdf";
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
-import { generateText } from "ai";
+import { generateObject, generateText } from "ai";
 import fs from "fs";
+import { z } from "zod";
 
 const zai = createOpenAICompatible({
   name: "ZAI",
@@ -9,45 +10,70 @@ const zai = createOpenAICompatible({
   baseURL: "https://open.bigmodel.cn/api/paas/v4/",
 });
 
-const extract_text_from_image = async (imagePath: string) => {
-  const { text } = await generateText({
-    model: zai("glm-4.6v-flash"),
-    system:
-      "Extract all readable text from this image. " +
-      "Pay special attention to titles, author names, publication information, " +
-      "and other metadata that would help identify this document.",
-    messages: [
-      {
-        role: "user",
-        content: [
-          { type: "text", text: "Extract texts." },
-          {
-            type: "image",
-            image: fs.readFileSync(imagePath),
-          },
-        ],
-      },
-    ],
-  });
-  return text;
-};
+const extractTextFromImage = async (imageData: Uint8Array) =>
+  (
+    await generateText({
+      model: zai("glm-4.6v-flash"),
+      system:
+        "Extract all readable text from this image. " +
+        "Pay special attention to titles, author names, publication information, " +
+        "and other metadata that would help identify this document.",
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: "Extract texts." },
+            {
+              type: "image",
+              image: imageData,
+            },
+          ],
+        },
+      ],
+    })
+  ).text;
 
-console.log(await extract_text_from_image("examples/img.png"));
+const determineDocType = async (text: string) =>
+  (
+    await generateObject({
+      model: zai("glm-4.5-flash"),
+      system: "Classify the document based on the cover text. The output should be one of 'article', 'book', or 'unknown'.",
+      // "Classify the document into one of the following types based on the cover text. " +
+      // "- 'article': Journal articles, conference papers, research papers, preprints " +
+      // "- 'book': Fiction books, non-fiction books, textbooks " +
+      // "- 'unknown': Neither of the above.",
+      prompt: text,
+      output: "enum",
+      enum: ["article", "book", "unknown"],
+    })
+  ).object;
 
-const extract_metadata_from_file = async (filePath: string) => {
-  const document = mupdf.PDFDocument.openDocument(fs.readFileSync(filePath));
-  const format = document.getMetaData("format");
-  const modificationDate = document.getMetaData("info:ModDate");
-  const author = document.getMetaData("info:Author");
+async function extractMetadataFromFile(filePath: string) {
+  const extension = filePath.split(".").pop()?.toLowerCase();
+  if (!["pdf", "epub", "mobi", "djvu"].includes(extension || "")) {
+    throw new Error(`Unsupported file type: ${extension}`);
+  }
+
+  let text: string;
+  let cover: Uint8Array;
+
+  if (extension === "djvu") {
+  }
+
+  const document = mupdf.Document.openDocument(fs.readFileSync(filePath));
   const numPages = document.countPages();
   const page = document.loadPage(0);
-  const cover = page
+  cover = page
     .toPixmap(mupdf.Matrix.identity, mupdf.ColorSpace.DeviceRGB)
     .asPNG();
-  const text = page.toStructuredText().asText();
-  console.log(
-    `format=${format}, modificationDate=${modificationDate}, author=${author}, numPages=${numPages}, text=${text}`
-  );
-};
+  // fs.writeFileSync("example/cover.png", cover);
+  text = page.toStructuredText().asText();
+  if (text.length < 100) {
+    text = await extractTextFromImage(cover);
+  }
 
-await extract_metadata_from_file("examples/ebook.epub");
+  const docType = await determineDocType(text);
+  console.log(`numPages=${numPages}, docType=${docType}, text=${text}`);
+}
+
+await extractMetadataFromFile("examples/ocr.pdf");
