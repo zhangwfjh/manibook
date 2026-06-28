4561d417cf7e87a603c5264351287bc5e45b0a16
Shaun Zhang <zhangwfjh@gmail.com>
2026-06-28 08:54:36 +0800

    chore(card): remove unused DocumentMetadata component

--- components/library/documents/index.ts
+++ components/library/documents/index.ts
@@ -2,4 +2,3 @@ export { DocumentCard } from "./card";
 export { DocumentCardSkeleton } from "./card-skeleton";
 export { DocumentActions } from "./document-actions";
 export { DocumentImage } from "./document-image";
-export { DocumentMetadata } from "./metadata";
